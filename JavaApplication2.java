62536bd8ae7da987a5bb90ff54a6cf3d5629b3da
Your Name <t5pevi00@students.oamk.fi>
2016-10-05 18:02:59 +0300

    javafile

---
+++ JavaApplication2.java
@@ -0,0 +1,23 @@
+public class main {
+public static void main(String[] args) {
+Product product = new Product("Edam", 3.3, 120);
+System.out.println("Product value is " + product.countValue());
+product.printProduct();
+}
+}
+class Product {
+private String name;
+private double price;
+private int amount; //Amount in storage
+public Product(String name, double price, int amount) {
+this.name = name;
+this.price = price;
+this.amount = amount;
+}
+public double countValue() {
+return(amount * price);
+}
+public void printProduct() {
+System.out.printf("Product %s, price %4.1f and balance %d pcs", name,price,amount);
+}
+}
