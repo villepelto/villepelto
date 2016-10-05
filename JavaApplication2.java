74c0a299654b1ec31f33187f5e6650bd3317eb14
Your Name <t5pevi00@students.oamk.fi>
2016-10-05 18:28:00 +0300

    javakek

--- JavaApplication2.java
+++ JavaApplication2.java
@@ -2,6 +2,7 @@ Author: Ville Pelto
 public class main {
 public static void main(String[] args) {
 Product product = new Product("Edam", 3.3, 120);
+changePrice();
 System.out.println("Product value is " + product.countValue());
 product.printProduct();
 }
@@ -21,4 +22,11 @@ return(amount * price);
 public void printProduct() {
 System.out.printf("Product %s, price %4.1f and balance %d pcs", name,price,amount);
 }
+}
+
+public void changePrice()
+{
+	
+	price += price;
+	
 }
